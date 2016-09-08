5105213c181d246e0736ce2e52c7e3830eb85bd0
ujjvalk <ujjvalk@gmail.com>
2016-09-08 11:14:57 +0530

    Remove unnecessary Name space

--- TestDemo/App_Start/BundleConfig.cs
+++ TestDemo/App_Start/BundleConfig.cs
@@ -1,8 +1,6 @@
-﻿using System.Web;
-using System.Web.Optimization;
-
-namespace TestDemo
+﻿namespace TestDemo
 {
+    using System.Web.Optimization;
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
