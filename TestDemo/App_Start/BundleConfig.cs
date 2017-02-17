a571b9a8f69386be59a57b3793883a08e7da19ce
ujjvalk <ujjvalk@gmail.com>
2017-02-17 19:34:34 +0530

    some Modification

--- TestDemo/App_Start/BundleConfig.cs
+++ TestDemo/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
