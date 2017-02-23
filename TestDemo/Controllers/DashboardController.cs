daf6a61fa3a9be7db3893240b74a086e34ac3d5f
ujjvalk <ujjvalk@gmail.com>
2017-02-23 16:23:37 +0530

    add Pie chart to dashboard

---
+++ TestDemo/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using TestDemo.Models;
+
+namespace TestDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult TransactionSummary()
+        {
+            using (var db = new TestDemoEntities())
+            {
+                var data = (from t in db.Transactions select new { t.Type }).ToList();
+                var egrp = data.Select(trans => trans.Type)
+               .GroupBy(x => x, (Transcation, transactionCount) => new { Value = Transcation, Item = transactionCount.Count() });
+                return Json(egrp.ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
