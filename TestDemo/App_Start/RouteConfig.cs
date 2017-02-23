7bffe173ba2e10f23b6109e14ebe8d2a66b0330b
ujjvalk <ujjvalk@gmail.com>
2017-02-23 16:24:39 +0530

    add Dashboard to start page and Change layout

--- TestDemo/App_Start/RouteConfig.cs
+++ TestDemo/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace TestDemo
@@ -16,7 +12,7 @@ namespace TestDemo
             routes.MapRoute(
                   name: "Default",
                   url: "{controller}/{action}/{id}",
-                  defaults: new { controller = "Employee", action = "Employee", id = UrlParameter.Optional }
+                  defaults: new { controller = "Dashboard", action = "index", id = UrlParameter.Optional }
               );
             
         }
